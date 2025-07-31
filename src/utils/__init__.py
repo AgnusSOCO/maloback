7ba286e0b52fe1bcc6ec14cfdd6508665e8e3a3d
SOCO PWA <juan@socopwa.com>
2025-07-31 14:15:46 -0700

    Update __init__.py

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,2 +1,18 @@
-# Utils package
+"""
+Utility modules for the loan platform
+"""
+
+from .auth import token_required, admin_required, generate_token
+from .database import init_database, migrate_schema
+from .helpers import get_client_ip, log_action
+
+__all__ = [
+    'token_required', 
+    'admin_required', 
+    'generate_token',
+    'init_database',
+    'migrate_schema',
+    'get_client_ip',
+    'log_action'
+]
 
